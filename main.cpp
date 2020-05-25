9562e28350f82e43c72c2f62d04de586b4783dbc
sgrzegorz <grzegorzsroka2@gmail.com>
2020-05-25 20:42:52 +0200

    start debugging

--- main.cpp
+++ main.cpp
@@ -3,68 +3,198 @@
 #include <iterator>
 #include <fstream>
 #include <sstream>
+#include <set>
+#include <tuple>
+
 
 
 using namespace std;
 
-class Vertex {
-public:
-    int x;
-    int y;
+//class Vertex {
+//public:
+//    int x;
+//    int y;
+//
+//    Vertex(int x, int y) {
+//        this->x = x;
+//        this->y = y;
+//    }
+//
+//};
+//
+//class Graph {
+//public:
+//    list<Vertex> vertices;
+//
+//};
 
-    Vertex(int x, int y) {
-        this->x = x;
-        this->y = y;
-    }
+//struct vertex {
+//    int x;
+//    int y;
+//    set<vertex> neighbours;
+//
+//};
+//
+//struct edge{
+//    vertex u;
+//    vertex v;
+//};
 
+struct Board{
+    char symbol;
+    int vertexId;
 };
 
-class Graph {
-public:
-    list<Vertex> vertices;
-
-};
-
-struct vertex {
-    int x;
-    int y;
-};
-
-
-char** board;
+Board ** board;
 int W, H, L, K;
-void showBoard(){
+int NVERTICES = 0;
+
+void showBoard(bool flag){
     for(int i=0;i<H;i++){
         for(int j=0;j<W;j++){
-            cout<<board[i][j]<<" ";
+            if(flag){
+                cout<<board[i][j].symbol<<" ";
+            } else{
+                if(board[i][j].vertexId==-1){
+                    cout<<". ";
+                }else{
+                    cout<<board[i][j].vertexId<<" ";
+                }
+            }
         }
         cout<<endl;
     }
 }
+class Vertex {
+public:
+    Vertex(int x,int y){
+        this->x =x;
+        this->y =y;
+    }
+    Vertex(){
+    }
+    int x;
+    int y;
+};
 
-
-int main() {
-
-//    ifstream infile("board");
-
+void readFromInput(string filename){
     string boardName;
-    ifstream file("input.txt");
+    ifstream file(filename);
     string line;
 
     file >> W >> H >>L>>K;
     file >>boardName;
 
-    board  = new char*[W];
+    board  = new Board*[W];
     for(int i = 0; i < W; ++i)
-        board[i] = new char[H];
+        board[i] = new Board[H];
 
     for(int i=0;i<H;i++){
         for(int j=0;j<W;j++){
-            file >> board[i][j];
+            char symbol;
+            file >> symbol;
+            if(symbol!='.'){
+                board[i][j].vertexId=NVERTICES;
+                NVERTICES++;
+            }else{
+                board[i][j].vertexId=-1;
+            }
+            board[i][j].symbol=symbol;
         }
     }
+}
 
-    showBoard();
+class Edge{
+public:
+    Edge(int x,int y){
+        this->x =min(x,y);
+        this->y = max(x,y);
+    }
+    int x;
+    int y;
+    bool operator<(const Edge& rhs) const
+    {
+        return std::tie(x, y) < std::tie(rhs.x, rhs.y);
+
+    }
+};
+
+
+set<int> *vertices;
+Vertex *verticesXY;
+set<Edge> edges;
+
+
+bool isValid(int x,int y){
+    return (x>=0 && x<W && y>=0 && y<H);
+}
+
+void resolveNewEdge(int x,int y,char symbol1,char symbol2, int vertexId){
+    if(isValid(x,y)){
+        Board other = board[x][y];
+        if(other.symbol==symbol1 || other.symbol==symbol2) edges.insert(Edge(vertexId,other.vertexId));
+    }
+}
+
+void makeEdgesFromBoard(){
+    for(int y=0;y<H;y++){
+        for(int x=0;x<W;x++){
+            char symbol = board[x][y].symbol;
+            if(symbol=='.') continue;
+            int vertexId = board[x][y].vertexId;
+
+            switch (symbol) {
+                case '+':
+                    resolveNewEdge(x,y-1,'+','|',vertexId);
+                    resolveNewEdge(x,y+1,'+','|',vertexId);
+                    resolveNewEdge(x-1,y,'+','-',vertexId);
+                    resolveNewEdge(x+1,y,'+','-',vertexId);
+                    break;
+                case '-':
+                    resolveNewEdge(x-1,y,'+','-',vertexId);
+                    resolveNewEdge(x+1,y,'+','-',vertexId);
+                    break;
+                case '|':
+                    resolveNewEdge(x,y-1,'+','|',vertexId);
+                    resolveNewEdge(x,y+1,'+','|',vertexId);
+                    break;
+            }
+        }
+    }
+}
+
+int main() {
+    readFromInput("input.txt");
+    makeEdgesFromBoard();
+
+//    int vertices_iter=0;
+//    vertices = new set<int>[NVERTICES];
+
+
+//    verticesXY = new  Vertex[NVERTICES];
+
+
+
+
+//
+//    for(int i=0;i<H;i++){
+//        for(int j=0;j<W;j++){
+//
+//            cout<<board[i][j]<<" ";
+//        }
+//        cout<<endl;
+//    }
+
+
+
+//    vertex *vertices;
+    showBoard(true);
+
+    showBoard(false);
+
+
+
+
 //    cout<<"h";
 
 
